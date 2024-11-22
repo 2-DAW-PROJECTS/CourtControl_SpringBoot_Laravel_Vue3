5898592c4c9984ff0e75ad73197250a5d4949179
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-11-22 11:17:31 +0100

    Migrate Lessons

--- backend/laravel/database/seeders/DatabaseSeeder.php
+++ backend/laravel/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(SportsTableSeeder::class);
+        $this->call(LessonsTableSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
