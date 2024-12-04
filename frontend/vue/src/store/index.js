f406bfdef57082f9f508843a21cd6ab37b9cbdb2
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-12-04 12:43:25 +0100

    Debugg for MaterialsDashboard & Materials

--- frontend/vue/src/store/index.js
+++ frontend/vue/src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex';
-import { sports } from './modules/client/sport';
 import { materials } from './modules/client/materials';
-
+import { materialDashboard } from './modules/dashboard/materialsDashboard';
 
 export default createStore({
     strict: process.env.NODE_ENV !== 'production',
@@ -27,7 +26,41 @@ export default createStore({
         isDashboardMode: state => state.dashboardMode
     },
     modules: {
-        sports,
         materials,
+        materialDashboard
     }
 });
+
+// import { createStore } from 'vuex';
+// import { sports } from './modules/client/sport';
+// import { materials } from './modules/client/materials';
+
+
+// export default createStore({
+//     strict: process.env.NODE_ENV !== 'production',
+//     state: {
+//         loading: false,
+//         error: null,
+//         dashboardMode: false
+//     },
+//     mutations: {
+//         SET_LOADING(state, status) {
+//             state.loading = status;
+//         },
+//         SET_ERROR(state, error) {
+//             state.error = error;
+//         },
+//         SET_DASHBOARD_MODE(state, mode) {
+//             state.dashboardMode = mode;
+//         }
+//     },
+//     getters: {
+//         isLoading: state => state.loading,
+//         getError: state => state.error,
+//         isDashboardMode: state => state.dashboardMode
+//     },
+//     modules: {
+//         sports,
+//         materials,
+//     }
+// });
