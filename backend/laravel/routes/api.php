85f4ee69154692907dea9a3cf1cbd95873b64824
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-12-18 09:18:51 +0100

    Check-Admin & Request

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\SportController;
 
 /*
@@ -22,11 +24,10 @@ use App\Http\Controllers\SportController;
 
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin/test', function () {
-        return response()->json(['message' => 'You are an admin']);
+        return response()->json(['message' => 'You are an admin nice cock bro']);
     });
     //aqui haremos la generacion para confirmar el token
 });
-
 
 // Rutas para el controlador SportController
 Route::get('/sports', [SportController::class, 'index']);
@@ -34,3 +35,6 @@ Route::get('/sports/{id}', [SportController::class, 'show']);
 Route::post('/sports', [SportController::class, 'store']);
 Route::put('/sports/{id}', [SportController::class, 'update']);
 Route::delete('/sports/{id}', [SportController::class, 'destroy']);
+
+// Ruta para verificar si el usuario es administrador
+Route::post('/users/check-admin', [UserController::class, 'checkAdmin']);
