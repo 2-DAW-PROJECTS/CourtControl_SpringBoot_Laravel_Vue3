e769824633378f8793aaa00386da7efa3468d2c0
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2024-12-18 12:15:08 +0100

    Comprobaciones hechas, el token se genera correctamente

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -4,28 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 use App\Http\Controllers\SportController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
 Route::middleware(['admin'])->group(function () {
-    Route::get('/admin/test', function () {
-        return response()->json(['message' => 'You are an admin nice cock bro']);
-    });
+    // Route::get('/admin/test', function () {
+    //     return response()->json(['message' => 'You are an admin nice cock bro']);
+    // });
+    Route::post('/admin/test', [AdminController::class, 'generateToken']);
     //aqui haremos la generacion para confirmar el token
 });
 
