33c628aa5fb539cecb6377e5686a93d301f8928c
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-01 19:31:47 +0100

    Home-Dashboards & Providers

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,57 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import DashboardsPage from './views/DashboardsPage';
-
+import DashboardPage from './views/DashboardsPage';
 
 function App() {
   return (
-    <Router>
-      <div id="app">
-        <Header />
-        <div className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<DashboardsPage />} />
-            {/* Puedes agregar más rutas aquí si es necesario */}
-          </Routes>
-        </div>
-        <Footer />
+    <div id="app">
+      <Header />
+      <div className="container mx-auto px-4 py-8">
+        <DashboardPage>
+          {/* <h1>Esto es una prueba de funcionamiento</h1> */}
+          {}
+        </DashboardPage>
       </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
 
-
-
 // import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
+
 // import Header from './components/layout/Header';
 // import Footer from './components/layout/Footer';
-// // import HomePage from './views/HomePage';
-// // import ProfilePage from './views/ProfilePage';
-// // import ShopPage from './views/ShopPage';
-// // import CourtDetails from './components/details/CourtDetails';
-// // import LessonDetails from './components/details/LessonDetails';
+// import DashboardsPage from './views/DashboardsPage';
 
 // function App() {
 //   return (
-//     <Router>
-//       <div id="app">
-//         <Header />
+//     <div id="app">
+//       <Header />
+//       <div className="container mx-auto px-4 py-8">
 //         <Routes>
-//           {/* <Route path="/" element={<HomePage />} />
-//           <Route path="/shop" element={<ShopPage />} />
-//           <Route path="/court/:id" element={<CourtDetails />} />
-//           <Route path="/lesson/:id" element={<LessonDetails />} />
-//           <Route path="/profile" element={<ProfilePage />} /> */}
+//           <Route path="/" element={<DashboardsPage />} />
+//           {/* Puedes agregar más rutas aquí si es necesario */}
 //         </Routes>
-//         <Footer />
 //       </div>
-//     </Router>
+//       <Footer />
+//     </div>
 //   );
 // }
 
