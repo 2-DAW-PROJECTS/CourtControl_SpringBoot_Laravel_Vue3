05e818ea0ab221b06352fa250e17735786dba59e
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-06 10:33:13 +0100

    Summer Update & create corregidos

--- backend/SpringBoot/src/main/java/com/alfosan_javi/spring/domain/service/SummerService.java
+++ backend/SpringBoot/src/main/java/com/alfosan_javi/spring/domain/service/SummerService.java
@@ -1,7 +1,11 @@
 package com.alfosan_javi.spring.domain.service;
 
+import com.alfosan_javi.spring.api.dto.SummerDTO;
 import com.alfosan_javi.spring.domain.model.Summer;
+import com.alfosan_javi.spring.domain.model.Sport;
 import com.alfosan_javi.spring.domain.repository.SummerRepository;
+import com.alfosan_javi.spring.domain.repository.SportRepository;
+import com.alfosan_javi.spring.api.assembler.SummerAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +14,14 @@ import java.util.Optional;
 
 @Service
 public class SummerService {
-
     @Autowired
     private SummerRepository summerRepository;
+
+    @Autowired
+    private SportRepository sportRepository;
+
+    @Autowired
+    private SummerAssembler summerAssembler;
 
     public List<Summer> getAllSummers() {
         return summerRepository.findAll();
@@ -22,8 +31,33 @@ public class SummerService {
         return summerRepository.findById(id);
     }
 
-    public Summer saveSummer(Summer summer) {
-        return summerRepository.save(summer);
+    public SummerDTO createSummer(SummerDTO summerDTO) {
+        Sport sport = sportRepository.findById(summerDTO.getIdSport()).orElse(null);
+        if (sport == null) {
+            return null;
+        }
+        Summer summer = summerAssembler.toEntity(summerDTO, sport);
+        Summer savedSummer = summerRepository.save(summer);
+        return summerAssembler.toModel(savedSummer);
+    }
+
+    public Summer updateSummer(long id, SummerDTO summerDTO) {
+        Optional<Summer> existingSummer = summerRepository.findById(id);
+        if (existingSummer.isPresent()) {
+            Summer summer = existingSummer.get();
+            summer.setSport(sportRepository.findById(summerDTO.getIdSport()).orElse(null));
+            summer.setImg(summerDTO.getImg());
+            summer.setNameSummer(summerDTO.getNameSummer());
+            summer.setCategoryAge(summerDTO.getCategoryAge());
+            summer.setStartDate(summerDTO.getStartDate());
+            summer.setEndDate(summerDTO.getEndDate());
+            summer.setMaxParticipants(summerDTO.getMaxParticipants());
+            summer.setDescription(summerDTO.getDescription());
+            summer.setActivities(summerDTO.getActivities());
+            summer.setIsActive(summerDTO.getIsActive());
+            return summerRepository.save(summer);
+        }
+        return null;
     }
 
     public boolean deleteSummer(long id) {
@@ -42,3 +76,73 @@ public class SummerService {
         return summerRepository.findBySportIds(sportIds);
     }
 }
+// package com.alfosan_javi.spring.domain.service;
+
+// import com.alfosan_javi.spring.domain.model.Summer;
+// import com.alfosan_javi.spring.domain.repository.SummerRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// @Service
+// public class SummerService {
+
+//     @Autowired
+//     private SummerRepository summerRepository;
+
+//     public List<Summer> getAllSummers() {
+//         return summerRepository.findAll();
+//     }
+
+//     public Optional<Summer> getSummerById(long id) {
+//         return summerRepository.findById(id);
+//     }
+
+//     public Summer saveSummer(Summer summer) {
+//         return summerRepository.save(summer);
+//     }
+
+// public SummerDTO createSummer(SummerDTO summerDTO) {
+//         Summer summer = summerAssembler.toEntity(summerDTO);
+//         Summer savedSummer = summerRepository.save(summer);
+//         return summerAssembler.toModel(savedSummer);
+//     }
+
+//     public Summer updateSummer(long id, SummerDTO summerDTO) {
+//         Optional<Summer> existingSummer = summerRepository.findById(id);
+//         if (existingSummer.isPresent()) {
+//             Summer summer = existingSummer.get();
+//             // Actualizar los campos de summer con los valores de summerDTO
+//             summer.setIdSport(summerDTO.getIdSport());
+//             summer.setImg(summerDTO.getImg());
+//             summer.setNameSummer(summerDTO.getNameSummer());
+//             summer.setCategoryAge(summerDTO.getCategoryAge());
+//             summer.setStartDate(summerDTO.getStartDate());
+//             summer.setEndDate(summerDTO.getEndDate());
+//             summer.setMaxParticipants(summerDTO.getMaxParticipants());
+//             summer.setDescription(summerDTO.getDescription());
+//             summer.setActivities(summerDTO.getActivities());
+//             summer.setIsActive(summerDTO.getIsActive());
+//             return summerRepository.save(summer);
+//         }
+//         return null;
+//     }
+
+//     public boolean deleteSummer(long id) {
+//         if (summerRepository.existsById(id)) {
+//             summerRepository.deleteById(id);
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     public boolean existsById(long id) {
+//         return summerRepository.existsById(id);
+//     }
+
+//     public List<Summer> getFilteredSummersBySport(List<Long> sportIds) {
+//         return summerRepository.findBySportIds(sportIds);
+//     }
+// }
