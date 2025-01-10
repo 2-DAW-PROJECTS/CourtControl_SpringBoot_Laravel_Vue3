8adb912f92e5527e66abda96b486d2607c1ea9ab
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-10 09:39:31 +0100

    Aplicar Guards

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { setTokens, fetchCurrentUser } from './store/slices/userSlice';
+import { setTokens } from './store/slices/userSlice';
 
-import Constants from './Constants';
+// import Constants from './Constants';
 
 import DashboardsPage from './views/DashboardsPage';
 import AdminHomePage from './views/AdminHomePage';
@@ -29,27 +29,27 @@ const App = () => {
     const dispatch = useDispatch();
     const accessToken = useSelector((state) => state.users.accessToken);
     const refreshToken = useSelector((state) => state.users.refreshToken);
-    const user = useSelector((state) => state.users.currentUser);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const encryptedAccessToken = urlParams.get('accessToken');
         const encryptedRefreshToken = urlParams.get('refreshToken');
+        const userParam = urlParams.get('user');
 
 
-        if (encryptedAccessToken && encryptedRefreshToken) {
+        if (encryptedAccessToken && encryptedRefreshToken && userParam) {
             try {
-            const accessToken = atob(encryptedAccessToken);
-            const refreshToken = atob(encryptedRefreshToken);
+            const accessTokenDecrypt = atob(encryptedAccessToken);
+            const refreshTokenDecrypr = atob(encryptedRefreshToken);
+            const user = JSON.parse(decodeURIComponent(userParam));
+                // console.log(accessToken);
+                // console.log(refreshToken);
+                // console.log(user);
+            dispatch(setTokens({ accessTokenDecrypt, refreshTokenDecrypr }));
 
-                console.log(accessToken);
-                console.log(refreshToken);
-
-                dispatch(setTokens({ accessToken, refreshToken }));
-                dispatch(fetchCurrentUser(accessToken));
-
-                localStorage.setItem('accessToken', accessToken);
-                localStorage.setItem('refreshToken', refreshToken);
+                localStorage.setItem('accessToken', accessTokenDecrypt);
+                localStorage.setItem('refreshToken', refreshTokenDecrypr);
+                localStorage.setItem('currentAdmin', JSON.stringify(user));
 
                 window.history.replaceState({}, document.title, window.location.pathname);
             } catch (error) {
@@ -58,95 +58,96 @@ const App = () => {
         }
     }, [dispatch]);
 
+    // const storetest = useSelector((state) => state.users.currentAdmin);
+    // console.log(storetest);
+
     return (
         <>
             <Header />
                 <Routes>
                     <Route path="/" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
                                 <AdminHomePage />
-                            {/* </AdminGuard> */}
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <AdminHomePage />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/:type" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <DashboardsPage />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/reservations/:type" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <ReservationsPage />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/courts/:id" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <CourtDetails />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/lessons/:id" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <LessonDetails />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/summers/:id" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <SummerDetails />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/users/:id" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <UserDetails />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/courts/create" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <CreateCourt />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/lessons/create" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <CreateLesson />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
 
                     <Route path="/admin/summers/create" element={
                         <TokenGuard token1={accessToken} token2={refreshToken}>
-                            {/* <AdminGuard user={user}> */}
+                            <AdminGuard user={localStorage.getItem('currentAdmin')}>
                                 <CreateSummer />
-                            {/* </AdminGuard> */}
+                            </AdminGuard>
                         </TokenGuard>
                     }/>
                 </Routes>
